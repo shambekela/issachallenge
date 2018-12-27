c0b893e4efc244b0bb970009c2703bd2488cba41
Toivo Toivo <toivo1996@gmail.com>
2018-12-27 20:46:41 +0200

    Minor changes

--- gunicorn-config.py
+++ gunicorn-config.py
@@ -2,6 +2,5 @@ from app import scheduler
 
 def when_ready(server):
 	server.log.info("Server is ready. Spawning workers")
-	if scheduler.state == 1:
-		scheduler.shutdown()
+	scheduler.shutdown()
 	scheduler.start()
